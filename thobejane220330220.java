61530daa47d164ec162336f87eb36b5690608436
Theophilus Thobejane <thobejanetheo@gmail.com>
2024-05-07 13:05:25 +0200

    this is our question 2 commit

---
+++ thobejane220330220.java
@@ -0,0 +1,10 @@
+
+public class thobejane220330220{
+	public static void main(String[]args){
+		int num1 = 5;
+		int num2 = 5;
+
+		int sum = num1 + num2;
+		System.out.print("your number is: " + sum);
+	}
+}
